d9f782ca7ab85212a1f8dcf162915f2e4f6d3a26
Norah <Norahmutairi@outlook.sa>
2021-09-13 11:58:17 +0300

    Add project files

---
+++ database/migrations/2021_09_11_102126_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id')->nullable();
+            $table->integer('author_id')->nullable();
+            $table->string('title');
+            $table->longText('content');
+            $table->enum('status',['pending','approved','cancelled'])->nullable()->default('pending');
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
